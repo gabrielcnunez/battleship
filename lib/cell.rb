8e3e2956d0823fb92f81117997bf1c6426387536
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-08-30 13:57:15 -0700

    Add place ship, fired upon, and fire upon methods, pass tests

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,12 +3,28 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
   def empty?
     if @ship == nil
       true
     else
       false
+    end
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+  
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    @fired_upon = true
+    if self.empty? == false
+      @ship.hit
     end
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -20,4 +20,31 @@ RSpec.describe do
       expect(cell.empty?).to eq(true)
     end
   end
+  describe '#place_ship' do
+    it 'can put ship in cells' do
+      cell = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+      expect(cell.ship).to eq(cruiser)
+      expect(cell.empty?).to eq(false)
+    end
+  end
+  describe '#fired_upon?' do
+    it 'can tell if cell is fired upon' do
+      cell = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+      expect(cell.fired_upon?).to eq(false)
+    end
+  end
+  describe '#fire_upon' do
+    it 'can fire upon a cell' do
+      cell = Cell.new("B4")
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+      cell.fire_upon
+      expect(cell.ship.health).to eq(2)
+      expect(cell.fired_upon?).to eq(true)
+    end
+  end
 end
