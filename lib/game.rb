157eb4144ce984da9ea1e797ffa96d9061197cdb
Will Wang <willy.j.wang@gmail,com>
2022-09-02 16:59:24 -0500

    Create place_computer_ships method, wip

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,10 @@
 class Game
-  
+  attr_reader :computer_board, :player_board
   def initialize
-    @computer_board = nil
-    @player_board = nil
+    @computer_board = Board.new
+    @player_board = Board.new
   end
-  
+
   def start_game
     self.print_start_message
     user_input = gets.chomp
@@ -15,10 +15,23 @@ class Game
     else
       "Invalid entry, please try again"
       self.start_game
+    end
   end
 
   def print_start_message
-    return "Welcome to BATTLESHIP\n" + "Enter p to play. Enter q to quit." 
+    return "Welcome to BATTLESHIP\n" + "Enter p to play. Enter q to quit."
   end
 
+  def random_coordinate(board)
+    board.cells.keys.sample
+  end
+
+  def place_computer_ships(ships)
+    coordinates = []
+    ships.each do |ship|
+      until @computer_board.valid_placement?(ship.name, coordinates) == true
+        coordinates = @computer_board.cells.keys.sample(ship.length)
+      end
+    end
+  end
 end
